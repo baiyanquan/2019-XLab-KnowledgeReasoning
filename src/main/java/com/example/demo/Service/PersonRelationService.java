68ee10ab5ea83bcf1b3ca97f765b67d2b61b4061
yan <1173054960@qq.com>
2019-10-04 23:09:31 +0800

    first week

--- src/main/java/com/example/demo/Service/PersonRelationService.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.Service;
-
-import com.example.demo.Entity.PersonRelation;
-
-public interface PersonRelationService {
-
-    PersonRelation addPersonRelation(String name1, String name2);
-    void deletePersonRelation(String name1,String name2);
-}
