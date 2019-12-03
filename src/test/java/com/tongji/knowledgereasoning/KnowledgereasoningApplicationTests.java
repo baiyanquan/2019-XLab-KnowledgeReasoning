7b89b01c39fe90a5b1b1995e506a37480d7fc7e2
double Z <doubleZ0108@163.com>
2019-12-03 20:12:23 +0800

    init empty springboot

---
+++ src/test/java/com/tongji/knowledgereasoning/KnowledgereasoningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tongji.knowledgereasoning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnowledgereasoningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
